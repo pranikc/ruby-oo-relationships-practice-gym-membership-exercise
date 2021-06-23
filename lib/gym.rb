b71a9065a5a27cd78ae56f3505cf91247d2893a2
Pranik Chainani <pranikc@gmail.com>
2021-06-23 15:35:15 -0400

    commit tree c37b8006b68b7543afbd8b1841a479b388356e59

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.gym == self}
+  end
+
+  def lifters
+    self.memberships.map{|membership| membership.lifter}
+  end
+
+  def names_of_lifters
+    self.lifters.map{|lifter| lifter.name}
+  end
+
+  def combined_lift_total
+    count = 0
+    self.lifters.each{|lifter| count += lifter.lift_total}
+    count
   end
 end
+
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,40 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
+  @@total = 0
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all_lift_total += lift_total
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def memberships
+    Membership.all.select{|membership| membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map{|membership| membership.gym}
+  end
+
+  def self.ave_lift_total
+    @@all_lift_total / self.all.length
+  end
+
+  def total_cost
+    total = 0
+    self.memberships.each{|membership| total += membership.cost}
+    total
+  end
+
+  def new_membership(gym, cost)
+    Membership.new(cost, gym, self)
   end
 end
+
